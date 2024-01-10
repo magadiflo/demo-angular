19fba1033a02dadfe24359062c8160168a2a4b35
Martín <magadiflo@gmail.com>
2024-01-09 23:41:29 -0500

    Header y Presentación finalizados

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,20 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faQuoteLeft, faSearch, faStar } from '@fortawesome/free-solid-svg-icons';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, FontAwesomeModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+
+  public faStar = faStar;
+  public faQuoteLeft = faQuoteLeft;
+  public faSearch = faSearch;
 
 }
